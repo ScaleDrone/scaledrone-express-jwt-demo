e757a9e95a4e3565cf69cf78caf765ebe5425b10
herkyl <s.herkyl@gmail.com>
2018-09-23 15:56:07 +0300

    Bring the example up to date

--- index.js
+++ index.js
@@ -1,43 +1,52 @@
-var express = require('express');
-var jwt = require('jwt-simple');
-var secret = process.env.SCALEDRONE_SECRET || 'fnrdXD2hu1NdzGFN1c1i8KfrcZbwxdR7';
-var channel = process.env.SCALEDRONE_CHANNEL || 'm7LXeWUig7QmXMqW';
-var app = express();
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const log = require('tracer').colorConsole();
+const {CHANNEL_ID, CHANNEL_SECRET} = process.env;
+const PORT = process.env.PORT || 1234;
 
-function hasChannelAccess(req) {
-    return true;
+if (!CHANNEL_ID) {
+  log.error('Please provide a CHANNEL_ID environmental variable');
+  process.exit();
+}
+if (!CHANNEL_SECRET) {
+  log.error('Please provide a CHANNEL_SECRET environmental variable');
+  process.exit();
 }
 
-app.get('/jwt', function(req, res) {
-    res.header('Access-Control-Allow-Origin', '*'); // for developing
-    if (hasChannelAccess(req)) {
-        console.log(req.query);
-        var clientId = req.query.clientId,
-            payload = {
-                client: clientId, // the client that wants to connect
-                channel: channel, // channel that the client want's to connect to
-                permissions: {
-                    '.*': { /* Regex matches everything */
-                        publish: false, /* Forbids publishing to all channels */
-                        subscribe: true /* Allows subscribing to all channels */
-                    },
-                    '^room1$': { /* Exact match to room1 */
-                        publish: true, /* Allows publishing in room1 */
-                        subscribe: true
-                    },
-                    '^room2$': {
-                        publish: true,
-                        subscribe: true
-                    }
-                },
-                exp: Date.now() + 180000 // client can use this token for 3 minutes (UTC0)
-            },
-            token = jwt.encode(payload, secret, 'HS256');
-        res.send(token);
-    } else {
-        res.send(403, 'Sorry! You are not allowed.');
-    }
+const app = express();
+app.set('view engine', 'ejs'); // EJS is used to inject channelID into the html file
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.render('index', {CHANNEL_ID});
 });
 
-app.listen(1234);
-console.log('Server is running..');
+app.get('/auth/:clientId', function(req, res) {
+  if (hasChannelAccess(req)) {
+    const payload = {
+      client: req.params.clientId,
+      channel: CHANNEL_ID,
+      permissions: {
+        '^myroom$': {
+          publish: true,
+          subscribe: true,
+        },
+      },
+      exp: Math.floor(Date.now() / 1000) + 60 * 3 // client has to use this token within 3 minutes
+    };
+    const token = jwt.sign(payload, CHANNEL_SECRET, {algorithm: 'HS256'});
+    res.status(200).end(token);
+  } else {
+    res.status(403).end('Sorry! You are not allowed.');
+  }
+});
+
+function hasChannelAccess(req) {
+  // Your should implement your own authentication code here.
+  // You could query your user from your database and see if they are allowed to
+  // connect or give them user-scoped access using JWT permissions
+  return true;
+}
+
+app.listen(PORT);
+log.info(`Server is running on port ${PORT}. Visit http://localhost:${PORT}`);
---
+++ readme.md
@@ -0,0 +1,17 @@
+# [Scaledrone](https://www.scaledrone.com/) Express.js JWT authentication example
+
+This example shows how to use JWT authentication in your project
+
+## Running the example
+
+Install the dependencies:
+```
+npm install
+```
+
+Create a new channel in Scaledrone and copy the ID and secret. Then start the server:
+```
+CHANNEL_ID=dJLd12d91d... CHANNEL_SECRET=8LI85gAt... npm start
+```
+
+Open up http://localhost:1234
